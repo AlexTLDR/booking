a97948fec4292d19b7cccb69f651f5891733d53f
AlexTLDR <alex.badragan@protonmail.com>
2024-05-30 11:52:39 +0200

    used UpdateUserParams struct for setting what can be updated

--- db/user_store.go
+++ db/user_store.go
@@ -16,7 +16,7 @@ type UserStore interface {
 	GetUsers(context.Context) ([]*types.User, error)
 	InsertUser(context.Context, *types.User) (*types.User, error)
 	DeleteUser(context.Context, string) error
-	UpdateUser(ctx context.Context, filter bson.M, update bson.M) error
+	UpdateUser(ctx context.Context, filter bson.M, params types.UpdateUserParams) error
 }
 
 type MongoUserStore struct {
@@ -31,10 +31,11 @@ func NewMongoUserStore(client *mongo.Client) *MongoUserStore {
 	}
 }
 
-func (s *MongoUserStore) UpdateUser(ctx context.Context, filter bson.M, values bson.M) error {
+func (s *MongoUserStore) UpdateUser(ctx context.Context, filter bson.M, params types.UpdateUserParams) error {
+
 	update := bson.D{
 		{
-			"$set", values,
+			"$set", params.ToBSON(),
 		},
 	}
 	_, err := s.coll.UpdateOne(ctx, filter, update)
--- types/user.go
+++ types/user.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"regexp"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -14,6 +15,11 @@ const (
 	minLastNameLen  = 2
 	minPasswordLen  = 8
 )
+
+type UpdateUserParams struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
 
 type CreateUserParams struct {
 	FirstName string `json:"firstName"`
@@ -28,6 +34,17 @@ type User struct {
 	LastName          string             `bson:"lastName" json:"lastName"`
 	Email             string             `bson:"email" json:"email"`
 	EncryptedPassword string             `bson:"EncryptedPassword" json:"-"`
+}
+
+func (p UpdateUserParams) ToBSON() bson.M {
+	m := bson.M{}
+	if len(p.FirstName) > 0 {
+		m["firstName"] = p.FirstName
+	}
+	if len(p.LastName) > 0 {
+		m["lastName"] = p.LastName
+	}
+	return m
 }
 
 func (params CreateUserParams) Validate() map[string]string {
